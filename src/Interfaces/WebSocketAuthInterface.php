0347803726b5c7ed55c1d2dd9e5a3e68c78b857b
layman <892349689@qq.com>
2025-06-12 15:33:57 +0800

    fix:auth

--- src/Interfaces/WebSocketAuthInterface.php
+++ src/Interfaces/WebSocketAuthInterface.php
@@ -5,7 +5,7 @@ interface WebSocketAuthInterface
     /**
      * 通过请求数据进行认证
      * @param array $query WebSocket 握手请求参数
-     * @return int|string  用户唯一标识
+     * @return int|null|string  用户唯一标识
      */
-    public function authenticate(array $query): int|string;
+    public function authenticate(array $query): int|null|string;
 }
